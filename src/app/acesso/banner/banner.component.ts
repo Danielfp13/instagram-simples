8e6a52faf74498176dc517b5048857eddc7ce4da
Daniel <daniel.si@outlook.com.br>
2021-05-07 22:19:23 -0300

    Testando primeira  animação

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,15 +1,35 @@
+import { style, trigger, state, transition, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  animations: [
+    trigger('banner', [
+      state('escondido', style({
+        opacity: 0
+      })),
+      state('visivel', style({
+        opacity: 1
+      })),
+      transition('escondido <=> visivel', animate('1s ease-in')),
+
+    ])
+  ]
 })
+
 export class BannerComponent implements OnInit {
+
+  public estado: string = 'escondido';
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public toogleEstadoAnimacao(): void{
+    this.estado = this.estado === 'visivel' ? 'escondido' : 'visivel'
+  }
 }
