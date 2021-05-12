fecb509f238fad7db75be8a0af559701865335fb
Daniel <daniel.si@outlook.com.br>
2021-05-12 10:34:53 -0300

    a

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-acesso',
@@ -20,12 +20,24 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
     trigger('animacao-painel', [
       state('criado', style({
         opacity: 1
-      }) ),
+      })),
       transition('void => criado', [
         style({
-          opacity:0, transform: 'translate(80px, 0px)'
+          opacity: 0, transform: 'translate(80px, 0px)'
         }),
-        animate('800ms 0s ease-in-out')
+        animate('1500ms 0s ease-in-out', keyframes([
+          style({ offset: 0.15, opacity: 1, transform: 'translateX(0)' }),
+          style({ offset: 0.86, opacity: 1, transform: 'translateX(0)' }),
+          
+          style({ offset: 0.88, opacity: 1, transform: 'translateY(-10px)' }),
+          style({ offset: 0.90, opacity: 1, transform: 'translateY(10px)' }),
+          style({ offset: 0.92, opacity: 1, transform: 'translateY(-10px)' }),
+          style({ offset: 0.94, opacity: 1, transform: 'translateY(10px)' }),
+          style({ offset: 0.96, opacity: 1, transform: 'translateY(-10px)' }),
+          style({ offset: 0.98, opacity: 1, transform: 'translateY(10px)' }),
+      
+          style({ offset: 1, opacity: 1, transform: 'translateY(0)' })
+        ]))
       ])
     ])
   ]
@@ -40,7 +52,7 @@ export class AcessoComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  public exibir(event: string): void{
+  public exibir(event: string): void {
     this.cadastro = event === 'cadastro' ? true : false
   }
 
