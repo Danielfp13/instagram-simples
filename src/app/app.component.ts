16a35f0984c7edd1b05e72530c3f33decc20794a
Daniel <daniel.si@outlook.com.br>
2021-05-13 15:09:10 -0300

    Conectando o Firebase SDK do projeto com o Back-end do Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
+import  firebase from "firebase/app";
+
+import "firebase/analytics";
+
+import "firebase/auth";
+import "firebase/firestore";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'instagram-simples';
+
+export class AppComponent implements OnInit {
+  title = 'instagram-clone';
+
+  ngOnInit(): void {
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyDY9xltqPN6HbmXZg_lHKIkkfcOWw530kM",
+      authDomain: "instagram-clone-a0828.firebaseapp.com",
+      projectId: "instagram-clone-a0828",
+      storageBucket: "instagram-clone-a0828.appspot.com",
+      messagingSenderId: "948394183546",
+      appId: "1:948394183546:web:8a8f10c3031ddcdf9364c3",
+      measurementId: "G-TF4E0EYKW8"
+    };
+
+    firebase.initializeApp(firebaseConfig)
+  }
 }
